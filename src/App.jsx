1c1902f274ea28be51a78c042d6cd96a90639a86
Danujainoshika <danujamanukularathna@gmail.com>
2025-11-27 11:54:12 +0530

    feat: implement CreateTicketDialog component for ticket creation

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,14 @@ import Test from './components/technician/Test'
 
 import LoginPage from './pages/loginPage'
 import { TestResidentDialog } from './components/resident/testResidentDialog';
+import { CreateTicketDialog } from './components/resident/CreateTicketDialog';
 
 function App() {
 
   return (
     <>
       
-      <TestResidentDialog/>
+      <CreateTicketDialog/>
     </>
   );
 }
